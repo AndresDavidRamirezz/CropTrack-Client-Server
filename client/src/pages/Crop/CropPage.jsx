facdf13f278c248f5f39fefce5486a08af4c6f71
Andres Ramirez <andresdavidramirezz97@gmail.com>
2026-01-04 18:14:05 -0300

    feature: Agregar rutas para CropPage.jsx, MeasurementPage.jsx, TaskPage.jsx, WorkerPage.jsx, ProfilePage.jsx, ExtrasPage.jsx

---
+++ client/src/pages/Crop/CropPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CropPage = () => {
+		return (
+				<h1>Bienvenido a la pagina de principal de Cosechas de CropTrack, aqui podras administrar todas tus cosechas.</h1>
+		);
+};
+
+export default CropPage;
---
+++ client/src/pages/Worker/WorkerPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const WorkerPage = () => {
+		return (
+				<h1>Bienvenido a la pagina de principal de Trabajadores de CropTrack, aqui podras administrar todos tus trabajadores.</h1>
+		);
+};
+
+export default WorkerPage;
