b9e5622698cbb47dd8ed10c459aa9993d8404cf3
Andres Ramirez <andresdavidramirezz97@gmail.com>
2026-01-04 17:46:42 -0300

    Agregar archivos MainPage.jsx y .css, NavigationButtons.jsx y .css, y ProtectedRoutes.jsx, con respectivos cambios en client/index.jsx y .css

---
+++ client/src/pages/MainPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import NavigationButtons from '../components/NavigationButtons/NavigationButtons';
+
+const MainPage = () => {
+  return (
+    <div>
+      <NavigationButtons />
+    </div>
+  );
+};
+
+export default MainPage;
